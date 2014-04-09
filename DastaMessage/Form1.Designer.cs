2a60a8f8bbbe596d2ea5e1595eaf5ec10386783b
Milan Polacek <polacek.emilan@gmail.com>
2014-04-09 11:50:40 +0200

    IniComit

---
+++ DastaMessage/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace DastaMessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnValdu = new System.Windows.Forms.Button();
+            this.btnReadZprava = new System.Windows.Forms.Button();
+            this.lblVal = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnValdu
+            // 
+            this.btnValdu.Location = new System.Drawing.Point(147, 24);
+            this.btnValdu.Name = "btnValdu";
+            this.btnValdu.Size = new System.Drawing.Size(128, 45);
+            this.btnValdu.TabIndex = 0;
+            this.btnValdu.Text = "Validate";
+            this.btnValdu.UseVisualStyleBackColor = true;
+            this.btnValdu.Click += new System.EventHandler(this.btnValdu_Click);
+            // 
+            // btnReadZprava
+            // 
+            this.btnReadZprava.Location = new System.Drawing.Point(41, 35);
+            this.btnReadZprava.Name = "btnReadZprava";
+            this.btnReadZprava.Size = new System.Drawing.Size(75, 23);
+            this.btnReadZprava.TabIndex = 1;
+            this.btnReadZprava.Text = "Otevri zpravu";
+            this.btnReadZprava.UseVisualStyleBackColor = true;
+            this.btnReadZprava.Click += new System.EventHandler(this.btnReadZprava_Click);
+            // 
+            // lblVal
+            // 
+            this.lblVal.AutoSize = true;
+            this.lblVal.Location = new System.Drawing.Point(41, 102);
+            this.lblVal.Name = "lblVal";
+            this.lblVal.Size = new System.Drawing.Size(84, 13);
+            this.lblVal.TabIndex = 2;
+            this.lblVal.Text = "Validator error ->";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(697, 376);
+            this.Controls.Add(this.lblVal);
+            this.Controls.Add(this.btnReadZprava);
+            this.Controls.Add(this.btnValdu);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnValdu;
+        private System.Windows.Forms.Button btnReadZprava;
+        private System.Windows.Forms.Label lblVal;
+    }
+}
+
---
+++ DastaMessage/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Linq;
+
+namespace DastaMessage
+{
+    public partial class Form1 : Form
+    {
+
+        XDocument xDocument;
+        FileStream streamDoc;
+
+        XmlTextReader tr;
+        List<string> valdaMessageData = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnValdu_Click(object sender, EventArgs e)
+        {
+            
+            XmlValidatingReader vr = new XmlValidatingReader(tr);
+
+            //vr.Schemas.Add(null, "./dasta_XML_schemes/ds_cistype-4.01.01.xsd");
+            //vr.Schemas.Add(null, "./dasta_XML_schemes/ds_dasta-4.02.04.xsd");
+            //vr.Schemas.Add(null, "./dasta_XML_schemes/ds_ip.xsd");
+            //vr.Schemas.Add(null, "./dasta_XML_schemes/ds_type-4.01.07.xsd");
+            vr.ValidationType = ValidationType.Schema;
+            vr.ValidationEventHandler += new ValidationEventHandler(ValidationHandler);
+
+            while (vr.Read()) ;
+
+            MessageBox.Show("Validation finished");
+            lblVal.Text = "Validator error -> ";
+            foreach (var err in valdaMessageData)
+            {
+                lblVal.Text+= err;
+            }
+        }
+
+        public void ValidationHandler(object sender, ValidationEventArgs args)
+        {
+            valdaMessageData.Add("***Validation error"+"\tSeverity: " + args.Severity.ToString()+"\tMessage: " + args.Message);
+        }
+
+        private void btnReadZprava_Click(object sender, EventArgs e)
+        {
+            string seznamCtenychSouboru = ListFileTerminalsOpen();
+
+            OpenFileDialog openFileDialogNemoci = new OpenFileDialog();
+
+            openFileDialogNemoci.Filter = seznamCtenychSouboru;
+            openFileDialogNemoci.RestoreDirectory = true;
+            openFileDialogNemoci.Title = "Otevri Zpravu";
+            if (openFileDialogNemoci.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    //streamDoc = new FileStream(openFileDialogNemoci.FileName, FileMode.Open);
+                    tr = new XmlTextReader(openFileDialogNemoci.FileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Pojebalo se to nekde pri nacitani \r\n Puvodni error: " + ex.Message);
+                }
+            }
+
+            //try
+            //{
+            //    xDocument.Elements();
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Nenacetl si dokument");
+            //    //throw new Exception("Nenacetl si dokument");
+            //}
+
+
+
+        }
+        private string ListFileTerminalsOpen()
+        {
+            List<string> cteneTypySouboru = new List<string>();
+            cteneTypySouboru.Add("XML files (*.xml)|*.xml");
+            cteneTypySouboru.Add("All files (*.*)|*.*");
+
+            string seznamCtenychSouboru = "";
+
+            for (int i = 0; i < cteneTypySouboru.Count(); i++)
+            {
+                if (i < cteneTypySouboru.Count() - 1)
+                { seznamCtenychSouboru += cteneTypySouboru[i] + "|"; }
+                else
+                { seznamCtenychSouboru += cteneTypySouboru[i]; }
+            }
+
+            return seznamCtenychSouboru;
+
+        }
+        private XDocument ReadDataFromXMLFile(string route)
+        {
+            return XDocument.Load(route);
+        }
+    }
+}
